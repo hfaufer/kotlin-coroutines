19425dcb1dc6167b69305673b41e192d72bc3be5
hufer <hufer@fritz.box>
2025-05-13 18:39:19 +0200

    feat: Add an example for runBlocking

--- src/main/kotlin/de/hu/kotlin/coroutines/Kt010_HelloCoroutine.kt
+++ src/main/kotlin/de/hu/kotlin/coroutines/Kt010_HelloCoroutine.kt
@@ -1,3 +1,18 @@
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import kotlin.time.Duration.Companion.seconds
+
 fun main() {
-  println("Kt010_HelloCoroutine")
+    println("Kt010_HelloCoroutine")
+    print("Hello")
+
+    // runBlocking runs a new coroutine in the current thread. That is, it blocks the current thread.
+    // The rest of this function will not be executed until the coroutine and thereby runBlocking returns.
+    runBlocking {
+        print(",")
+        delay(3.seconds)
+        print(" world")
+    }
+
+    print("!")
 }
