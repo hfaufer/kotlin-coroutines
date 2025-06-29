265c1c5f52eecfdfa378c873ffa7d6a3315c45c8
hufer <hufer@fritz.box>
2025-06-29 04:06:28 +0200

    feat: Show the name of current thread.

--- src/main/kotlin/de/hu/kotlin/coroutines/Kt010_HelloCoroutine.kt
+++ src/main/kotlin/de/hu/kotlin/coroutines/Kt010_HelloCoroutine.kt
@@ -4,15 +4,21 @@ import kotlin.time.Duration.Companion.seconds
 
 fun main() {
     println("Kt010_HelloCoroutine")
-    print("Hello")
+    show("Hello")
 
     // runBlocking runs a new coroutine in the current thread. That is, it blocks the current thread.
     // The rest of this function will not be executed until the coroutine and thereby runBlocking returns.
     runBlocking {
-        print(",")
+        show(",")
         delay(3.seconds)
-        print(" world")
+        show(" world")
     }
 
-    print("!")
+    show("!")
 }
+
+fun show(message: String) {
+    var threadName = Thread.currentThread().getName()
+    System.out.printf("Thread [%20s]: %s%n", threadName, message)
+}
+
