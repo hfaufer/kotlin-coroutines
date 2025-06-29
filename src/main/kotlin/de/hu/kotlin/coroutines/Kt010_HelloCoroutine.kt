6ca28338cdf753e7924fa9e55d5ee184b2ff7d64
hufer <hufer@fritz.box>
2025-06-29 10:22:17 +0200

    refactor: Extract logging to singleton Log.

--- src/main/kotlin/de/hu/kotlin/coroutines/Kt010_HelloCoroutine.kt
+++ src/main/kotlin/de/hu/kotlin/coroutines/Kt010_HelloCoroutine.kt
@@ -2,32 +2,20 @@ package de.hu.kotlin.coroutines
 
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
-import java.nio.file.Files
-import java.util.logging.Logger
-import kotlin.io.path.Path
 import kotlin.time.Duration.Companion.seconds
-
-val logger: Logger = Logger.getLogger("de.hu.kotlin.coroutines.Kt010_HelloCoroutine")
 
 fun main() {
     println("Kt010_HelloCoroutine")
-    val configFile = System.getProperty("java.util.logging.config.file")
-    println("$configFile exists? " + Files.exists(Path(configFile)))
-    log("Hello")
+    Log.info("Hello")
 
     // runBlocking() executes the given code block in a new coroutine that runs in the current thread.
     // That is, runBlocking() blocks the current thread until the coroutine return.
     // As you will see in the output, all code is executed by the same thread.
     runBlocking {
-        log(",")
+        Log.info(",")
         delay(3.seconds) // delay() is a suspend function that can only be called in a coroutine.
-        log(" world")
+        Log.info("world")
     }
 
-    log("!")
-}
-
-fun log(message: String) {
-    val threadName = Thread.currentThread().name
-    logger.info("[$threadName]: $message")
+    Log.info("!")
 }
---
+++ src/main/kotlin/de/hu/kotlin/coroutines/Log.kt
@@ -0,0 +1,26 @@
+package de.hu.kotlin.coroutines
+
+import java.nio.file.Files
+import java.util.logging.Logger
+import kotlin.io.path.Path
+
+internal val logger: Logger = Logger.getLogger("de.hu.kotlin.coroutines")
+
+internal object Log {
+    init {
+        val propertyKey = "java.util.logging.config.file"
+        val configFile: String? = System.getProperty(propertyKey)
+        if (configFile == null) {
+            println("WARNING: Property $propertyKey not set!")
+        } else if (!Files.exists(Path(configFile))) {
+            println("ERROR: Configuration file '$propertyKey' not found!")
+        }
+    }
+
+    fun info(message: String): Log {
+        logger.info("[${threadName()}]: $message")
+        return this
+    }
+
+    private fun threadName() = Thread.currentThread().name
+}
