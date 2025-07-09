d37a6005e1acd17facca7d9b6a58bc7da9df7072
Hartmut Ufer <Hartmut.Ufer@vipion.de>
2025-07-09 08:05:39 +0200

    feat: Add better example for async/await that really returns a result.

--- src/main/kotlin/de/hu/kotlin/concurrency/AsyncDemo.kt
+++ src/main/kotlin/de/hu/kotlin/concurrency/AsyncDemo.kt
@@ -1,62 +1,56 @@
 package de.hu.kotlin.concurrency
 
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.asCoroutineDispatcher
+import de.hu.kotlin.coroutines.Log
+import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
-import java.util.concurrent.Executors
+import kotlin.random.Random
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.milliseconds
-import kotlin.time.toJavaDuration
 
 /*
  * Demonstrates that coroutines built with `async` run interleaved in a single thread.
  *
- * When you replace `delay` with `Thread.sleep` in AsyncCounter.run(), the coroutines
+ * When you replace `delay` with `Thread.sleep` in AsyncRandomSum.compute(), the coroutines
  * will be executed one after another, because `Thread.sleep` blocks the single thread
  * of execution.
  *
  * After replacing `delay` with `Thread.sleep`, you can execute the outer coroutine
- * with `runBlocking(Dispatchers.IO)` to see that now the two counters run in parallel
+ * with `runBlocking(Dispatchers.IO)` to see that now the two computations run in parallel
  * again, because this time they are executed in two different (platform) threads.
  * When running on JVM 21 or above, you can also use `runBlocking(Dispatchers.VT)` to
  * see the same effect. But now it uses virtual threads instead of platform threads.
  */
 fun main() {
     println("Hello, Async World!")
-    // Try replacing `delay` with `Thread.sleep` to see the difference in execution order.
-    // runBlocking(Dispatchers.IO) {
-    //runBlocking(Dispatchers.VT) {
+    //    runBlocking(Dispatchers.IO) {
+    //    runBlocking(Dispatchers.VT) {
     runBlocking {
-        val counter1 = async { AsyncCounter("Counter-1", 1500.milliseconds, end = 10).run() }
-        val counter2 = async { AsyncCounter("Counter-2", 1000.milliseconds, end = 15).run() }
-        counter1.await()
-        counter2.await()
+        val counter1: Deferred<Int> = async { AsyncRandomSum("Sum-1", 1500.milliseconds, steps = 10).compute() }
+        val counter2: Deferred<Int> = async { AsyncRandomSum("Sum-2", 1000.milliseconds, steps = 15).compute() }
+        Log.info("Launched async jobs ${counter1.key} and ${counter2.key}.")
+        val result = counter1.await() + counter2.await()
+        Log.info("Result is $result.")
     }
 }
 
-class AsyncCounter(
+class AsyncRandomSum(
     private val name: String,
     private val delay: Duration,
-    private val start: Int = 1,
-    private val end: Int = 12,
+    private val steps: Int,
 ) {
 
-    suspend fun run() {
-        (start..end).forEach { i ->
-            val threadName = Thread.currentThread().name
-            println("[$threadName] $name: $i")
+    suspend fun compute(): Int {
+        var sum = 0
+        (1..steps).forEach { i ->
+            Log.info("$name step $i")
+            sum += Random.nextInt(10)
             // Try replacing `delay` with `Thread.sleep` to see the difference in execution order.
             delay(delay)
             // Thread.sleep(delay.toJavaDuration())
         }
+        Log.info("$name: Result is $sum")
+        return sum
     }
 }
-
-private val Dispatchers.VT: CoroutineDispatcher
-    get() {
-        // In a real application, you would use Dispatchers.VT from kotlinx.coroutines.
-        return Executors.newVirtualThreadPerTaskExecutor().asCoroutineDispatcher()
-    }
