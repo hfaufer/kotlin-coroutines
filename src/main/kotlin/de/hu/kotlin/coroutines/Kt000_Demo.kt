87f398912a1d64a87427990b6c42f09f60d02e09
hufer <hufer@fritz.box>
2025-07-24 03:55:11 +0200

    doc: Add notes on GlobalScope.launch()

--- src/main/kotlin/de/hu/kotlin/coroutines/Kt000_Demo.kt
+++ src/main/kotlin/de/hu/kotlin/coroutines/Kt000_Demo.kt
@@ -7,6 +7,16 @@ import java.lang.Thread.sleep
 import java.time.Duration
 import kotlin.time.Duration.Companion.seconds
 
+/**
+ * Demonstrates executing a "bare" coroutine with GlobalScope.launch().
+ * However, this low-level approach creates a coroutine that is not governed
+ * by structured concurrency. Coroutines launched on the global scope can’t
+ * be automatically cancelled and aren’t aware of any life cycle. This means
+ * it’s very easy to introduce resource leaks, or to still execute a coroutine
+ * who's work is no longer needed.
+ * One use case for GlobalScope.launch() is to launch a top-level background
+ * processes that must stay active for the whole lifetime of an application.
+ */
 fun main() {
     val job = GlobalScope.launch {
         println("Hello")
@@ -15,3 +25,4 @@ fun main() {
     }
     sleep(Duration.ofSeconds(2))
 }
+
