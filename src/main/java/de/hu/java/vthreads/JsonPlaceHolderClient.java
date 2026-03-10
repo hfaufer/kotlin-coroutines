b01105ffb43cb2559566bcbe310c54aea4b0b9cb
Hartmut Ufer <Hartmut.Ufer@vipion.de>
2026-03-10 20:30:49 +0100

    feat: Add example for HTTP request with CompletableFuture in Java.

--- src/main/java/de/hu/java/vthreads/JsonPlaceHolderClient.java
+++ src/main/java/de/hu/java/vthreads/JsonPlaceHolderClient.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import de.hu.java.vthreads.user.User;
 
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -13,29 +14,38 @@ import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.time.Duration;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.logging.Logger;
 
 public class JsonPlaceHolderClient {
     private static final URI BASE_URL = URI.create("https://jsonplaceholder.typicode.com");
     private static final Logger logger = Logger.getLogger(JsonPlaceHolderClient.class.getName());
 
-    public static void main(final String... args) throws Exception {
-        final List<User> users = new JsonPlaceHolderClient().getUsers();
-        System.out.println(users);
+    static void main() throws Exception {
+        IO.println("====== Synchronous GET request ======");
+        final List<User> syncUsers= new JsonPlaceHolderClient().getUsers();
+        IO.println("Sync users: " + syncUsers);
+
+        IO.println("====== Asynchronous GET request ======");
+        getUsersAsync()
+                .thenAccept(users -> IO.println("Async users: " + users))
+                .join();
     }
 
     public List<User> getUsers() throws IOException, InterruptedException {
         try (var client = newHttp2Client()) {
-            final var request = HttpRequest.newBuilder()
-                    .uri(BASE_URL.resolve("/users"))
-                    .timeout(Duration.ofMinutes(1))
-                    .header("Accept", "application/json")
-                    .GET()
-                    .build();
+            final var request = getUsersRequest();
             final HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
             logger.fine(() -> "Response: " + response.body());
             logger.fine(() -> "Status code: " + response.statusCode());
             return deserializeUsers(response.body());
+        }
+    }
+
+    public static CompletableFuture<List<User>> getUsersAsync() {
+        try (var client = newHttp2Client()) {
+            return client.sendAsync(getUsersRequest(), BodyHandlers.ofString())
+                    .thenApply(response -> deserializeUsers(response.body()));
         }
     }
 
@@ -47,9 +57,21 @@ public class JsonPlaceHolderClient {
                 .build();
     }
 
-    private static List<User> deserializeUsers(final String json) throws JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.readValue(json, new TypeReference<List<User>>() {
-        });
+    private static HttpRequest getUsersRequest() {
+        return HttpRequest.newBuilder()
+                .uri(BASE_URL.resolve("/users"))
+                .timeout(Duration.ofMinutes(1))
+                .header("Accept", "application/json")
+                .GET()
+                .build();
+    }
+
+    private static List<User> deserializeUsers(final String json) {
+        final var objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.readValue(json, new TypeReference<>() {});
+        } catch (JsonProcessingException e) {
+            throw new UncheckedIOException(e);
+        }
     }
 }
